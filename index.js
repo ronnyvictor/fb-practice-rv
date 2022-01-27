e46cde4b4bffe575982c25e6db519ccfe1e11f7a
Roneil Victor <roneilvictor@Ronnys-MacBook-Pro.local>
2022-01-27 10:59:06 -0500

    imported restaurants.json

---
+++ index.js
@@ -0,0 +1,10 @@
+// Import restaurants
+const restaurants = require('./restaurants.json');
+
+console.log(restaurants[0].name)
+
+// Connect to Fire Store
+
+// Create a Collection called 'restaurants'
+
+// Add each restaurant
